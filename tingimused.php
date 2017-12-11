a440cf90954038498fbcfbf49acc4e38d27a9eb8
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2017-12-11 11:32:25 +0200

    pidu

--- tingimused.php
+++ tingimused.php
@@ -17,3 +17,20 @@ if ($vanus >= 0 and $vanus < 11) {
     echo 'Oled seenior, kes on '.$vanus.'aastat vana';
 }
 echo '<br />';
+
+$sokolaad = 'Kalev';
+switch ($sokolaad){
+    case 'Kalev';
+        echo 'Super sokolaad';
+        break;
+    case 'Kinder';
+        echo 'Laste lemmik';
+        break;
+    default:
+        echo 'Sobib kui muud pole';
+        break;
+}
+echo'<br />';
+$pidu = 'halb';
+$pidutseme = ($pidu == 'hea') ? 'lähme peole' : 'isuteme kodus';
+echo $pidutseme. '<br />';
