ae2f55d5c90fde2e99d95c9f56b9190d50e6db4d
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2017-12-11 09:53:35 +0200

    Fail, mis defineerib ja valjastab meie eesnime.

---
+++ mina.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drew-brenet.oispuu
+ * Date: 11.12.2017
+ * Time: 9:35
+ */
+$brenet = 'Brenet';
+
+echo 'Minu nimi on '.$brenet.'.<br />';
+
+print 'Minu nimi on '.$brenet.'.<br />';
+print ("Minu nimi on $brenet<br />");
+?>
