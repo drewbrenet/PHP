9ba757f926fce5e7d8654503e116636d4b2cee4d
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2017-12-18 13:09:21 +0200

    arvutus

--- arvsumma.php
+++ arvsumma.php
@@ -21,3 +21,21 @@ for ($kord = 1; $kord <= 5; $kord++) {
     $number = rand(0, 1000);
     echo 'Numbrid ' . $number . ' arvude summa on ' . arvuSumma($number) . '<br/>';
 }
+
+
+
+function otsiNumber($suvalineArv, $kindelArv){
+    echo $kindelArv. ' esined numbris '.$suvalineArv;
+    $mituKorda = 0;
+    while($suvalineArv != 0) {
+        $arv = $suvalineArv % 10;
+        if ($arv == $kindelArv){
+            $mituKorda++;
+        }
+        $suvalineArv = $suvalineArv / 10;
+        settype($suvalineArv, 'integer');
+    }
+    echo ' '.$mituKorda.', arv korda<br/>';
+}
+
+otsiNumber(442158755745, 5);
